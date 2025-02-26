888fbc8e4991f1d28f46272561ea3a71d058c1b6
benard00118 <benardopeter4@gmail.com>
2025-02-26 10:34:09 +0300

    Minor changes

--- research_portal/research/models.py
+++ research_portal/research/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
+class NewsItem(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='news_images/')
+    date = models.DateTimeField()
+    description = models.TextField()
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+
