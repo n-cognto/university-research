a8e7b6c914869b4f4721d27c81e570d48197f517
Benard Karanja <152717293+n-cognto@users.noreply.github.com>
2025-02-26 19:24:45 +0300

    Update urls.py

--- research_portal/profiles/urls.py
+++ research_portal/profiles/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import RegisterView, PasswordResetView, login_view, user_dashboard
+from .views import RegisterView, PasswordResetView, login_view
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -12,7 +12,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', login_view, name='login'),
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('user/dashboard/', user_dashboard, name='user_dashboard'),
 ]
 
 if settings.DEBUG:
