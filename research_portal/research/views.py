b2316959f3209805e37427354ca2367f5ef5de44
benard00118 <benardopeter4@gmail.com>
2025-02-26 10:34:35 +0300

    Minor changes in index view

--- research_portal/research/views.py
+++ research_portal/research/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import NewsItem
 
 def index(request):
-    return render(request, 'index.html')
+    news_items = NewsItem.objects.all().order_by('-date')
+    return render(request, 'index.html', {'news_items': news_items})
 
 def contact(request):
     return render(request, 'contact/contact.html')
+
+
+
+    
+
