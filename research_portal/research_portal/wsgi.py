8efce687279dfcd8534aeacd79309fe99dcebbd3
benard00118 <benardopeter4@gmail.com>
2025-03-02 12:40:44 +0300

    implemented whitenoise to serve static files

--- research_portal/research_portal/wsgi.py
+++ research_portal/research_portal/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research_portal.settings')
 
 application = get_wsgi_application()
+
+# Add WhiteNoise to serve static files
+from whitenoise import WhiteNoise
+from django.conf import settings  # Add this import
+
+application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, 'staticfiles'))
+application.add_files(os.path.join(settings.BASE_DIR, 'static'), prefix='static/')
