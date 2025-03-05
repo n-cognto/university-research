cd285b3a7bd2dcd8e47c0262a6f29694a9b3959d
benard00118 <benardopeter4@gmail.com>
2025-03-05 15:59:58 +0300

    Add profile and edit profile url

--- research_portal/profiles/urls.py
+++ research_portal/profiles/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import RegisterView, PasswordResetView, login_view, dashboard_view
+from .views import RegisterView, PasswordResetView, login_view, dashboard_view,profile_view, edit_profile, export_profile
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth import views as auth_views
@@ -15,6 +15,9 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='profiles/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='profiles/password_reset_complete.html'), name='password_reset_complete'),
     path('dashboard/', login_required(dashboard_view), name='dashboard'),
+    path('profile/', profile_view, name='profile'),
+    path('profile/export/<str:export_format>/', export_profile, name='export_profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
 ]
 
 
