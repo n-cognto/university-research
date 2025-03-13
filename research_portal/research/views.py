b130f843e5f10cc12de8ea2034c37992c86c336d
dee-mee <derekmuriuki2@gmail.com>
2025-03-13 14:02:06 +0300

    created vlirous page

--- research_portal/research/views.py
+++ research_portal/research/views.py
@@ -18,3 +18,27 @@ def map_view(request):
 def people(request):
     return render(request, 'contact/people.html')
 
+
+def vlirous(request):
+    return render(request, 'research/vlirous.html')
+
+def about(request):
+    return render(request, 'research/about.html')
+
+def sub_projects(request):
+    return render(request, 'research/sub_projects.html')
+
+def research(request):
+    return render(request, 'research/research.html')
+
+def community(request):
+    return render(request, 'research/community.html')
+
+def contact(request):
+    return render(request, 'contact/contact.html')
+
+def latest_activities(request):
+    return render(request, 'research/latest_activities.html')
+
+def sub_project_detail(request, project_id):
+    return render(request, 'research/sub_project_detail.html', {'project_id': project_id})
