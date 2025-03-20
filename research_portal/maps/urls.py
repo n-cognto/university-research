9bc7fa5a1bc75ce38c37d4fcf72a51c23e501824
benard00118 <benardopeter4@gmail.com>
2025-03-20 07:27:03 +0300

    Minor changes

--- research_portal/maps/urls.py
+++ research_portal/maps/urls.py
@@ -1,4 +1,3 @@
-# urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
@@ -12,13 +11,13 @@ router.register(r'weather-alerts', views.WeatherAlertViewSet)
 app_name = 'maps'
 
 urlpatterns = [
-    path('maps/', views.MapView.as_view(), name='map'),  
+    path('', views.MapView.as_view(), name='index'),
+    path('map/', views.MapView.as_view(), name='map'),  
     path('api/', include((router.urls, 'api'))),  
     path('api/debug/stations/', views.debug_stations, name='debug_stations'),
-
     path('csv-upload/', CSVUploadView.as_view(), name='csv_upload'),
     path('upload/success/', ImportSuccessView.as_view(), name='import_success'),
     path('flash-drive-import/', flash_drive_import_view, name='flash_drive_import'),
     path('api/map-data/', views.map_data, name='map_data'),
-    
+    path('api/import-csv/', views.api_import_csv, name='api_import_csv'),
 ]
