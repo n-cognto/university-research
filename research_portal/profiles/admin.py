0874b21ab4307567dbfba56a4baba9d57f987c5e
dee-mee <derekmuriuki2@gmail.com>
2025-03-21 13:15:45 +0300

    fixed login form and added groups

--- research_portal/profiles/admin.py
+++ research_portal/profiles/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
+from django.utils import timezone
 
-# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+    fields = ('middle_name', 'phone_number', 'id_number', 'dob', 'gender', 'location', 'bio')
+    widgets = {
+        'dob': admin.widgets.AdminDateWidget(attrs={'type': 'date', 'max': timezone.now().date().isoformat()}),
+    }
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_select_related = ('profile',)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
