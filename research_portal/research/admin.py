6bcf0444b4ef3ecc22a8bdcc7a52012f5e8b56f7
n-cognto <benardopeter4@gmail.com>
2025-05-18 13:26:57 +0300

    Minor changes

--- research_portal/research/admin.py
+++ research_portal/research/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import NewsItem
 
+@admin.register(NewsItem)
 class NewsItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'date')
     search_fields = ('title', 'description')
-    list_filter = ('date',)
-
-admin.site.register(NewsItem, NewsItemAdmin)
+    date_hierarchy = 'date'
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'date')
+        }),
+        ('Content', {
+            'fields': ('image', 'description')
+        }),
+    )
