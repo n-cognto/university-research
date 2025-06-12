1602afebd5256bc9e3820b7815c16b2e883681fb
n-cognto <benardopeter4@gmail.com>
2025-06-12 08:43:53 +0300

    Minor setttings changes

--- research_portal/settings.py
+++ research_portal/settings.py
@@ -102,23 +102,15 @@ WSGI_APPLICATION = 'research_portal.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'geomap_db',
         'USER': 'geomap_user',
         'PASSWORD': 'securepassword',  
         'HOST': 'localhost',
-        'PORT': '5432',  
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
